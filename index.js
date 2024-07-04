2d0577538db3b9819c7579f0df6608e5f6494d76
JOÃO VINÍCIUS DE AQUINO FARIAS <joao_va_farias@estudante.sesisenai.org.br>
2024-07-04 15:21:34 -0300

    cursinho

---
+++ index.js
@@ -0,0 +1,23 @@
+// function filtrar_unidades_Pares(p){
+//     if(p % 2 == 0){
+//       return "O número "+p+" é par"
+//     }else{
+//        return "O número "+p+" é ímpar"
+//     }
+// }
+// console.log(filtrar_unidades_Pares(9))
+
+// function filtrarPares(array){
+//     novo_array = []
+//     for(var i = 0; i<array.length; i++){
+//         if(array[i] % 2 == 0){
+//             novo_array.push(array[i])
+//         }
+//     }
+//     return novo_array
+// }
+// console.log(filtrarPares(array =[1,2,3,4,5,6]))
+
+
+
+
