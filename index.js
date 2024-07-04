b0f04f904278a35ecb6e9f9d4a226d84ba317200
João Vinicius de Aquino Farias <131161400+ApolloMorningstar@users.noreply.github.com>
2024-07-04 16:35:54 -0300

    commit "2"

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-// function filtrar_unidades_Pares(p){
-//     if(p % 2 == 0){
-//       return "O número "+p+" é par"
-//     }else{
-//        return "O número "+p+" é ímpar"
-//     }
-// }
-// console.log(filtrar_unidades_Pares(9))
+const express = require('express')
 
-// function filtrarPares(array){
-//     novo_array = []
-//     for(var i = 0; i<array.length; i++){
-//         if(array[i] % 2 == 0){
-//             novo_array.push(array[i])
-//         }
-//     }
-//     return novo_array
-// }
-// console.log(filtrarPares(array =[1,2,3,4,5,6]))
+const app = express()
 
-
-
-
+app.get('/test-api', function(req, res){
+    res.send("Nossa API está funcionando")
+})
+app.listen(8000)
