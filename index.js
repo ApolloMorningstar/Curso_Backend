6017d26dfdbbee30a752ea07c3e8be46605f871f
João Vinicius de Aquino Farias <131161400+ApolloMorningstar@users.noreply.github.com>
2024-07-04 17:35:30 -0300

    commit "3"

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const {Client} = require('pg')
+require("dotenv").config()
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+
 
 const app = express()
+
+const connectDB = async () =>{
+    client.connect()
+    .then(() => {
+        console.log('A conexão funcionou')
+}).catch((err) => {
+    console.error('Erro ao conectar na DB')
+})
+}
+connectDB()
 
 app.get('/test-api', function(req, res){
     res.send("Nossa API está funcionando")
